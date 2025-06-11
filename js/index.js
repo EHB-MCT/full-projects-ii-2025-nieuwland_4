bcf3af6a738d2070912a947cbc170ab774bc4c7d
Bichiran Gondal <bichiran.gondal@student.ehb.be>
2025-06-11 21:47:24 +0200

    home- navigatie, hamburger menu, banner, quicklinks, slideshow

--- js/index.js
+++ js/index.js
@@ -24,4 +24,10 @@ nextBtn.addEventListener('click', () => {
 
 ///////HAMBURGER MENU////////
 
+const hamburger = document.querySelector('.hamburgerBtn');
+const menu = document.querySelector('.mobileMenu');
 
+hamburger.addEventListener('click', () => {
+   menu.classList.toggle('show');
+   hamburger.classList.toggle('rotate');
+})
