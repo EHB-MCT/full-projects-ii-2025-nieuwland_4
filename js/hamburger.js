d7c4786996ba287af3b05d9a944a0994dc5beffe
Bichiran Gondal <bichiran.gondal@student.ehb.be>
2025-06-13 03:40:31 +0200

    responsive

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,7 @@
 ///////HAMBURGER MENU////////
 
 const hamburger = document.querySelector('.hamburgerBtn');
-const menu = document.querySelector('.mobileMenu');
+const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
    menu.classList.toggle('show');
